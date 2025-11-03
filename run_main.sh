450b121f5409a1e0a7a89a12c7e075571fb2b200
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-03 20:11:59 +0100

    invalid main?

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,6 @@
 # source .venv/bin/activate
 
-cd ./answering_questions
+cd answering_questions
 # python main.py --simulation_path /data0/sebastian.cavada/datasets/simulations/dl3dv \
 #     --destination_simulation_path /scratch/project/eu-25-92/composite_physics/dataset/physbench/simulation/dl3dv
 

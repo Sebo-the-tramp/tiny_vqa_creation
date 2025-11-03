33225189330a6e56644f1fb7ea0d279d161b7467
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-03 21:27:05 +0100

    updating subsample

--- run_subsample_10k.sh
+++ run_subsample_10k.sh
@@ -1,6 +1,6 @@
 cd answering_questions
 python subsample_questions.py \
-    --input ../output/test_full.json \
-    --output ../output/test.json \
-    --count 100 \
+    --input ../output/test_run03_full.json \
+    --output ../output/test_run03_10K.json \
+    --count 10000 \
     --seed 42

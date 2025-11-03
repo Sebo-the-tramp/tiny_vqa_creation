73e86af58280af32605b9ebb0f5a21dbad397985
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-03 19:42:29 +0100

    changing sh for karo

--- run_augment.sh
+++ run_augment.sh
@@ -1,2 +1,2 @@
 cd ./answering_questions
-python augment_kinematics.py --search-root /data0/sebastian.cavada/datasets/simulations/dl3dv
+python augment_kinematics.py --search-root /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3
--- run_main.sh
+++ run_main.sh
@@ -1,5 +1,11 @@
 source .venv/bin/activate
 
 cd ./answering_questions
-python main.py --simulation_path /data0/sebastian.cavada/datasets/simulations/dl3dv \
-    --destination_simulation_path /scratch/project/eu-25-92/composite_physics/dataset/physbench/simulation/dl3dv
+# python main.py --simulation_path /data0/sebastian.cavada/datasets/simulations/dl3dv \
+#     --destination_simulation_path /scratch/project/eu-25-92/composite_physics/dataset/physbench/simulation/dl3dv
+
+
+python main_parallel.py --simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
+    --destination_simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
+    --output_path ./output/test_karo.json \
+    --export_format json

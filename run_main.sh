92598992ae4f080ae5cd676e39e1895fdc577328
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-03 21:14:09 +0100

    updating to parallel

--- run_main.sh
+++ run_main.sh
@@ -4,8 +4,8 @@ cd answering_questions
 # python main.py --simulation_path /data0/sebastian.cavada/datasets/simulations/dl3dv \
 #     --destination_simulation_path /scratch/project/eu-25-92/composite_physics/dataset/physbench/simulation/dl3dv
 
-python main.py --simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
-    --export_format json
-
-# python main_parallel.py --simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
+# python main.py --simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
 #     --export_format json
+
+python main_parallel.py --simulation_path /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 \
+    --export_format json

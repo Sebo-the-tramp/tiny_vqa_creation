ded2569328938eb802a6ef4e79d48e35c29d5c91
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-04 18:07:47 +0100

    fixing augment

--- run_augment.sh
+++ run_augment.sh
@@ -1,5 +1,12 @@
-cd ./answering_questions
-python augment_kinematics_v3.py /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 --max-workers 36
 
-# cd ./answering_questions
-# python augment_kinematics_v3.py /data0/sebastian.cavada/datasets/simulations_v3 --max-workers 36
+if [ -d "/scratch/project/eu-25-92/composite_physics/dataset/simulation_v3" ]; then
+    echo "Directory exists. I AM on KARO"
+    cd ./answering_questions
+    python augment_kinematics_v3.py /scratch/project/eu-25-92/composite_physics/dataset/simulation_v3 --max-workers 128
+fi
+
+if [ -d "/data0/sebastian.cavada/datasets/simulations_v3" ]; then
+    echo "Directory exists. I AM on CavadaLAB"
+    cd ./answering_questions
+    python augment_kinematics_v3.py /data0/sebastian.cavada/datasets/simulations_v3 --max-workers 36
+fi

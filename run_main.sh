cbb2bb49704c85e8e4ae79bf5f5c95a3776cd2b5
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-07 16:47:08 +0100

    adding the balancing and changing some the main file

--- run_main.sh
+++ run_main.sh
@@ -18,11 +18,47 @@ cd answering_questions
 
 GENERAL_RUN_COUNT=10
 
+# # 10K general # text - no circling
+# python main_parallel.py --simulation_path "${BASE_PATH}/random/" \
+#     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
+#     --export_format json --run_name "run_${GENERAL_RUN_COUNT}_general" \
+#     --n_scenes 700
+
+# python ./subsample_questions_percentage.py \
+#     --count 10000 \
+#     --input ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general.json \
+#     --output ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_10K.json \
+#     --percentage-map ./balancing_sub_categories.json \
+#     --seed 42
+
+# RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
+# cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+# sed -i "s#/data0/sebastian.cavada/datasets/simulations_v3#/scratch/project/eu-25-92/composite_physics/dataset/simulation_v3#g" ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+
 # 10K general # text - no circling
-python main_parallel.py --simulation_path "${BASE_PATH}/random/" \
+python main_parallel.py --simulation_path "${BASE_PATH}/random-cam-stationary/" \
     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
-    --export_format json --run_name "run_${GENERAL_RUN_COUNT}_general" \
+    --export_format json --run_name "run_${GENERAL_RUN_COUNT}_general_cam-stationary" \
     --n_scenes 700
+
+# python ./subsample_questions_percentage.py \
+#     --count 10000 \
+#     --input ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_cam-stationary.json \
+#     --output ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_cam-stationary_10K.json \
+#     --percentage-map ./balancing_sub_categories.json \
+#     --seed 42
+
+# RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
+# cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_cam-stationary_karo_10K.json
+# sed -i "s#/data0/sebastian.cavada/datasets/simulations_v3#/scratch/project/eu-25-92/composite_physics/dataset/simulation_v3#g" ../output/$RUN_NAME/test_${RUN_NAME}_cam-stationary_karo_10K.json
+
+
+# rsync -avz -e "ssh -i ~/.ssh/id_rsa_karolina" \
+#   --include="*run_${GENERAL_RUN_COUNT}_*/***" \
+#   --exclude="*" \
+#   ../output/ \
+#   it4i-thvu@login2.karolina.it4i.cz:/mnt/proj1/eu-25-92/tiny_vqa_creation/output/ \
+
 
 # # 10K general # text - no circling
 # python main_parallel.py --simulation_path "${BASE_PATH}/random-cam-stationary/" \
@@ -95,13 +131,7 @@ python main_parallel.py --simulation_path "${BASE_PATH}/random/" \
 
 ### BALANCING THE RUNS ###
 
-# general 10K
-# python ./subsample_questions_percentage.py \
-#     --count 10000 \
-#     --input ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general.json \
-#     --output ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_10K.json \
-#     --percentage-map ./balancing_sub_categories.json \
-#     --seed 42
+
 
 # # 1K roi circling
 # python subsample_questions_percentage.py \

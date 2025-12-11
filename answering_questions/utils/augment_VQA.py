57e602fb7874abc8051f478fdb6d9823b46e9157
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-11 07:45:56 +0100

    fixing classes

--- answering_questions/utils/augment_VQA.py
+++ answering_questions/utils/augment_VQA.py
@@ -108,8 +108,8 @@ def augment_roi_circling(question, world_state, resolved_attributes, file_names,
                 render_name = file.split("/")[-1]
                 instance_image_path = file.replace("render", "instances")
 
-                print(world_state["encoding"])
-                rgb_object_class = world_state["encoding"]["classes"][
+                # print(world_state["encoding"])
+                rgb_object_class = world_state["encoding"]["semantic_classes"][
                     int(object_id) + 1
                 ]
 

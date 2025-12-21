b75b9176d66eeafc794842a03c6ef02ca24eb23c
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-21 14:49:48 +0100

    added output name

--- answering_questions/level_difficulty/generate.sh
+++ answering_questions/level_difficulty/generate.sh
@@ -3,13 +3,13 @@
 if [ -d "/scratch/project/eu-25-92/composite_physics/dataset/simulation_v4" ]; then
     echo "Directory exists. I AM on KARO"
     INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/test_run_11_general_10K.json"
-    OUTPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/"
+    OUTPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/test_run_11_general_levels.json"
 fi
 
 if [ -d "/data0/sebastian.cavada/datasets/simulations_v4" ]; then
     echo "Directory exists. I AM on CavadaLAB"
-    INPUT_PATH="--input /data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/output/run_10_general/test_run_10_general_10K.json"
-    OUTPUT_PATH="/data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/output/run_10_general/"
+    INPUT_PATH="/data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/output/run_10_general/test_run_10_general_10K.json"
+    OUTPUT_PATH="/data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/output/run_10_general/test_run_10_general_levels.json"
 fi
 
 python generate_levels_questions.py \

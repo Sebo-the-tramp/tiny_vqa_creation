7d8d1e9722d2ef2fc967a159d440a6a19bf8781a
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-21 18:52:38 +0100

    adding counterfactuals

--- run_main.sh
+++ run_main.sh
@@ -96,11 +96,11 @@ GENERAL_RUN_COUNT=11
 
 # -------------------------------------------------------------
 # 1K general # text counterfactual shift
-# python main_parallel_counterfactual.py --simulation_path "${BASE_PATH_CF}/shift-x" "${BASE_PATH_CF}/shift-z" \
-#     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
-#     --export_format json --run_name "run_${GENERAL_RUN_COUNT}_counterfactual_shift" \
-#     --counterfactual_type "shift" \
-#     --n_scenes 100
+python main_parallel_counterfactual.py --simulation_path "${BASE_PATH_CF}/shift-x" "${BASE_PATH_CF}/shift-z" \
+    --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
+    --export_format json --run_name "run_${GENERAL_RUN_COUNT}_counterfactual_shift" \
+    --counterfactual_type "shift" \
+    --n_scenes 1000
     
 # -------------------------------------------------------------
 # 1K general # text counterfactual gravity
@@ -108,15 +108,15 @@ python main_parallel_counterfactual.py --simulation_path "${BASE_PATH_CF}/low-gr
     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
     --export_format json --run_name "run_${GENERAL_RUN_COUNT}_counterfactual_gravity" \
     --counterfactual_type "gravity" \
-    --n_scenes 100
+    --n_scenes 1000
 
 # # -------------------------------------------------------------
 # # 1K general # text counterfactual gravity
-# python main_parallel_counterfactual.py --simulation_path "${BASE_PATH_CF}/2xsmaller" \
-#     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
-#     --export_format json --run_name "run_${GENERAL_RUN_COUNT}_counterfactual_smaller" \
-#     --counterfactual_type "volume" \
-#     --n_scenes 100
+python main_parallel_counterfactual.py --simulation_path "${BASE_PATH_CF}/2xsmaller" \
+    --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
+    --export_format json --run_name "run_${GENERAL_RUN_COUNT}_counterfactual_smaller" \
+    --counterfactual_type "volume" \
+    --n_scenes 1000
 
 # -------------------------------------------------------------
 # ABLATION STUDYs

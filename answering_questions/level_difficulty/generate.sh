fe6b137a9a6bedc720a282552ccbedb18bbc0581
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-21 14:45:33 +0100

    changing the correct path

--- answering_questions/level_difficulty/generate.sh
+++ answering_questions/level_difficulty/generate.sh
@@ -2,7 +2,7 @@
 
 if [ -d "/scratch/project/eu-25-92/composite_physics/dataset/simulation_v4" ]; then
     echo "Directory exists. I AM on KARO"
-    INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/test_run_10_general_10K.json"
+    INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/test_run_11_general_10K.json"
     OUTPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/"
 fi
 

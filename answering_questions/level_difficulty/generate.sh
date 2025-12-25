602a195d1fde3ac9b6dee4aff9c44ee1728e1853
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-25 12:04:18 +0100

    path again changed

--- answering_questions/level_difficulty/generate.sh
+++ answering_questions/level_difficulty/generate.sh
@@ -5,16 +5,16 @@ INPUT_FILE="test_run_11_general"
 
 if [ -d "/scratch/project/eu-25-92/composite_physics/dataset/simulation_v4" ]; then
     echo "Directory exists. I AM on KARO"
-    BASE_INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_11_general/"
-    INPUT_PATH="${BASE_INPUT_PATH}${INPUT_FILE}_10K.json"
-    OUTPUT_PATH="${BASE_INPUT_PATH}${INPUT_FILE}_levels.json"
+    BASE_INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output"
+    INPUT_PATH="${BASE_INPUT_PATH}/run_11_general/${INPUT_FILE}_10K.json"
+    OUTPUT_PATH="${BASE_INPUT_PATH}/run_11_general_levels/${INPUT_FILE}_levels.json"
 fi
 
 if [ -d "/data0/sebastian.cavada/datasets/simulations_v4" ]; then
     echo "Directory exists. I AM on CavadaLAB"
     BASE_INPUT_PATH="/data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/output/run_11_general/"
-    INPUT_PATH="${BASE_INPUT_PATH}${INPUT_FILE}_10K.json"
-    OUTPUT_PATH="${BASE_INPUT_PATH}${INPUT_FILE}_levels.json"
+    INPUT_PATH="${BASE_INPUT_PATH}/run_11_general/${INPUT_FILE}_10K.json"
+    OUTPUT_PATH="${BASE_INPUT_PATH}/run_11_general_levels/${INPUT_FILE}_levels.json"
 fi
 
 python generate_levels_questions.py \
@@ -22,5 +22,5 @@ python generate_levels_questions.py \
   --output "$OUTPUT_PATH" \
   --max-questions 1000
 
-cp "${BASE_INPUT_PATH}val_answer_run_11_general.json" \
-   "${BASE_INPUT_PATH}val_answer_run_11_general_levels.json"
+cp "${BASE_INPUT_PATH}/run_11_general/val_answer_run_11_general.json" \
+   "${BASE_INPUT_PATH}/run_11_general_levels/val_answer_run_11_general_levels.json"

bbc56768a8f65c3db10e492ca8acd265d394156f
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-29 16:46:06 +0100

    excluding wrong simulations

--- run_main.sh
+++ run_main.sh
@@ -31,18 +31,18 @@ python main_parallel.py --simulation_path "${BASE_PATH}/random/" \
     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
     --export_format json --run_name "run_${GENERAL_RUN_COUNT}_general" \
     --n_scenes 2200 \
-    --exclude_simulations_file "/data0/sebastian.cavada/compositional-physics/tiny_vqa_deterministic/answering_questions/problematic_paths.txt"
+    --exclude_simulations_file "./answering_questions/problematic_paths.txt"
 
-# python ./subsample_questions_percentage.py \
-#     --count 10000 \
-#     --input ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general.json \
-#     --output ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_10K.json \
-#     --percentage-map ./balancing_sub_categories.json \
-#     --seed 42
+python ./subsample_questions_percentage.py \
+    --count 10000 \
+    --input ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general.json \
+    --output ../output/run_${GENERAL_RUN_COUNT}_general/test_run_${GENERAL_RUN_COUNT}_general_10K.json \
+    --percentage-map ./balancing_sub_categories.json \
+    --seed 42
 
-# RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
-# cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
-# sed -i "s#/data0/sebastian.cavada/datasets/simulations_v3#/scratch/project/eu-25-92/composite_physics/dataset/simulation_v3#g" ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
+cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+sed -i "s#/data0/sebastian.cavada/datasets/simulations_v3#/scratch/project/eu-25-92/composite_physics/dataset/simulation_v3#g" ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
 
 
 # -------------------------------------------------------------

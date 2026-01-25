8142d3de9442137885e83cb614bcb109c6b41cd2
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2026-01-25 22:27:54 +0100

    different main

--- run_main.sh
+++ run_main.sh
@@ -30,25 +30,25 @@ GENERAL_RUN_COUNT=25
 
 ####################
 
-# python main_parallel.py --simulation_path "${BASE_PATH}/random/" "${BASE_PATH}/random-cam-stationary/" \
-#     --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
-#     --run_name "run_${GENERAL_RUN_COUNT}_general" \
-#     --n_scenes 5000 \
-#     --exclude_simulations_file "problematic_paths.txt" \
-#     --n_proc $CPUS \
-#     --timeit \
-#     --print_errors \
-#     # --include_categories "spatial_reasoning" \
+python main_parallel.py --simulation_path "${BASE_PATH}/random/" "${BASE_PATH}/random-cam-stationary/" \
+    --destination_simulation_path ${DESTINATION_SIMULATION_PATH} \
+    --run_name "run_${GENERAL_RUN_COUNT}_general" \
+    --n_scenes 5000 \
+    --exclude_simulations_file "problematic_paths.txt" \
+    --n_proc $CPUS \
+    --timeit \
+    --print_errors \
+    # --include_categories "spatial_reasoning" \
 
 RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
-# python ./subsample_questions_percentage.py \
-#     --count 10000 \
-#     --input ../output/${RUN_NAME}/test_${RUN_NAME}.json \
-#     --output ../output/${RUN_NAME}/test_${RUN_NAME}_10K.json \
-#     --percentage-map ./balancing_sub_categories.json \
-#     --seed 42
+python ./subsample_questions_percentage.py \
+    --count 10000 \
+    --input ../output/${RUN_NAME}/test_${RUN_NAME}.json \
+    --output ../output/${RUN_NAME}/test_${RUN_NAME}_10K.json \
+    --percentage-map ./balancing_sub_categories.json \
+    --seed 42
 
-# cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
 
 # RUN_NAME_OBJ="run_${GENERAL_RUN_COUNT}_general_obj_num"
 # python ./subsample_questions_numbers.py \
@@ -275,32 +275,32 @@ RUN_NAME="run_${GENERAL_RUN_COUNT}_general"
 # # -------------------------------------------------------------
 # # 1K roi circling - BASELINE
 
-RUN_NAME="run_${GENERAL_RUN_COUNT}_roi_ablation_baseline"
+# RUN_NAME="run_${GENERAL_RUN_COUNT}_roi_ablation_baseline"
 
-python main_parallel.py --simulation_paths "${BASE_PATH}/random" \
-    --per_object_count 100 \
-    --destination_simulation_path ${DESTINATION_SIMULATION_PATH}_modified_images \
-    --run_name "${RUN_NAME}" \
-    --include_categories "material_understanding" \
-    --augmentation "ablation" \
-    --exclude_question_ids "F_MASS_HEAVIEST_OBJECT" "F_MASS_LIGHTEST_OBJECT" "F_PHYSICS_PROPERTY_DENSITY_OBJECT_RELATIVE" \
-    "F_PHYSICS_PROPERTY_YOUNG_MODULUS_OBJECT_SIMILAR" "F_PHYSICS_PROPERTY_YOUNG_MODULUS_OBJECT_SIMILAR_NON_TECHNICAL" \
-    "F_PHYSICS_PROPERTY_YOUNG_MODULUS_HIGHEST" "F_PHYSICS_PROPERTY_YOUNG_MODULUS_HIGHEST_NON_TECHNICAL" "F_PHYSICS_PROPERTY_POISSON_RATIO_OBJECT_SIMILAR" \
-    "F_PHYSICS_PROPERTY_POISSON_RATIO_OBJECT_SIMILAR_NON_TECHNICAL" "F_MATERIAL_IDENTIFICATION_SIMILAR_OBJECT" \
-    "F_PHYSICS_PROPERTY_POISSON_RATIO_HIGHEST" "F_PHYSICS_PROPERTY_POISSON_RATIO_HIGHEST_NON_TECHNICAL" \
-    --n_proc $CPUS
+# python main_parallel.py --simulation_paths "${BASE_PATH}/random" \
+#     --per_object_count 100 \
+#     --destination_simulation_path ${DESTINATION_SIMULATION_PATH}_modified_images \
+#     --run_name "${RUN_NAME}" \
+#     --include_categories "material_understanding" \
+#     --augmentation "ablation" \
+#     --exclude_question_ids "F_MASS_HEAVIEST_OBJECT" "F_MASS_LIGHTEST_OBJECT" "F_PHYSICS_PROPERTY_DENSITY_OBJECT_RELATIVE" \
+#     "F_PHYSICS_PROPERTY_YOUNG_MODULUS_OBJECT_SIMILAR" "F_PHYSICS_PROPERTY_YOUNG_MODULUS_OBJECT_SIMILAR_NON_TECHNICAL" \
+#     "F_PHYSICS_PROPERTY_YOUNG_MODULUS_HIGHEST" "F_PHYSICS_PROPERTY_YOUNG_MODULUS_HIGHEST_NON_TECHNICAL" "F_PHYSICS_PROPERTY_POISSON_RATIO_OBJECT_SIMILAR" \
+#     "F_PHYSICS_PROPERTY_POISSON_RATIO_OBJECT_SIMILAR_NON_TECHNICAL" "F_MATERIAL_IDENTIFICATION_SIMILAR_OBJECT" \
+#     "F_PHYSICS_PROPERTY_POISSON_RATIO_HIGHEST" "F_PHYSICS_PROPERTY_POISSON_RATIO_HIGHEST_NON_TECHNICAL" \
+#     --n_proc $CPUS
 
-python ./subsample_questions_percentage.py \
-    --count 1000 \
-    --input ../output/${RUN_NAME}/test_${RUN_NAME}.json \
-    --output ../output/${RUN_NAME}/test_${RUN_NAME}_10K.json \
-    --percentage-map ./balancing_sub_categories_material_only.json \
-    --objects-per-count 100 \
-    --percentages-within-objects \
-    --soft-objects-per-count \
-    --seed 42
+# python ./subsample_questions_percentage.py \
+#     --count 1000 \
+#     --input ../output/${RUN_NAME}/test_${RUN_NAME}.json \
+#     --output ../output/${RUN_NAME}/test_${RUN_NAME}_10K.json \
+#     --percentage-map ./balancing_sub_categories_material_only.json \
+#     --objects-per-count 100 \
+#     --percentages-within-objects \
+#     --soft-objects-per-count \
+#     --seed 42
 
-cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
+# cp ../output/$RUN_NAME/test_${RUN_NAME}_10K.json ../output/$RUN_NAME/test_${RUN_NAME}_karo_10K.json
 
 
 

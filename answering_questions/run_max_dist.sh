00c286feb48b0839b7ba56ac8c864e971ef4a731
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2026-01-25 11:38:22 +0100

    path modified

--- answering_questions/run_max_dist.sh
+++ answering_questions/run_max_dist.sh
@@ -6,7 +6,7 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
-INPUT_PATH="$1"
+INPUT_PATH="/mnt/proj1/eu-25-92/tiny_vqa_creation/output/run_24_general/test_run_24_general.json"
 MODE_ARG=()
 if [[ $# -ge 2 && -n "${2:-}" ]]; then
   MODE_ARG=(--mode "$2")
